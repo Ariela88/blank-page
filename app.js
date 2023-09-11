581fc2b9e8c076acedb233eddf6bd1c3abcedc0a
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-09-11 12:33:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -89,6 +89,20 @@ function countChars() {
     return trimmedText.length;
 }
 
+document.onclick = (event) => {
+    if (document.fullscreenElement) {
+      document
+        .exitFullscreen()
+        .then(() => console.log("Document Exited from Full screen mode"))
+        .catch((err) => console.error(err));
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  };
+
+  function exitFullscreen(){
+    
+  }
 
 
 function updateCharCount() {
